cdcc0ba44e271fdfab46b866ecc91f5d63b7107b
Asafe <guilhermeasafemeireles@gmail.com>
2024-09-21 18:32:52 -0300

    estudo_auto

--- C_completoEtotal_cap2/const_modificador.c
+++ C_completoEtotal_cap2/const_modificador.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
+//Codigo apresentera erros na linha 11, devido a modificador const
 void recebe_const_mod(const int valor_1)
 {
 
 
         printf("%d\n",valor_1);
 
-        //Codigo apresentera erros na linha
         valor_1 = 20;
 
         printf("%d\n",valor_1);
