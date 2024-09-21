ddf19dd629c5399d79fc858380f2ea84135c800a
Asafe <guilhermeasafemeireles@gmail.com>
2024-09-21 18:31:14 -0300

    estudo_auto

--- C_completoEtotal_cap2/const_modificador.c
+++ C_completoEtotal_cap2/const_modificador.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-//
+
 void recebe_const_mod(const int valor_1)
 {
 
+
         printf("%d\n",valor_1);
 
-        valor_1 == 20;
+        //Codigo apresentera erros na linha
+        valor_1 = 20;
 
-        /*Nao condigo alterar o valor_1 devido ao const*/
-        //valor_1 == 20;
-        
         printf("%d\n",valor_1);
         
 
@@ -20,9 +19,10 @@ void recebe_const_mod(const int valor_1)
 int main()
 {
 
+
         int valor = 10;
 
         recebe_const_mod(valor);
 
-      
+
 }
